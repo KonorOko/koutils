c942592efed86bfa78d60dcd1fa762a960d30e58
KonorOko <125145871+KonorOko@users.noreply.github.com>
2024-01-01 20:19:26 -0600

    update

--- koutils/fast.py
+++ koutils/fast.py
@@ -1,42 +1,63 @@
-def sp(value: int, units: str = None, name: str = None):
-  '''
-  Convert a number to scientific notation
-  '''
-  sv = f'{value:.3e}'.split('e')
-  ss = sv[0] + '*10^' + sv[1]
-  if units is None and name is not None:
-    print(name, '=', ss)
-  elif name is None and units is not None:
-    print(ss, units)
-  elif name is None and units is None:
-    print(ss)
-  else:
-    print(name, '=', ss, units)
+# Project
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy as np
 
-def lrp(x: list, y: list, title: str = None):
-  '''
-  Make a linear regression and plot the result
-  '''
-  import matplotlib.pyplot as plt
-  from sklearn.linear_model import LinearRegression
-  import pandas as pd
-  df = pd.DataFrame(data={'x': x, 'y': y})
-  x = df[['x']]
-  y = df[['y']]
-  clf = LinearRegression()
-  clf.fit(x, y)
-  r = clf.predict(x)
-  m = clf.coef_
-  b = clf.intercept_
-  fig, ax = plt.subplots()
-  ax.plot(x, y, label='Original')
-  ax.scatter(x,y)
-  ax.set_title(f"y = {m[0][0]:.3} * x + {b[0]:.3}", loc='right',fontsize=10)
-  if title is None:
-    fig.suptitle('Linear Regression')
-  else:
-    fig.suptitle(f'{title}')
-  ax.plot(x, r, linestyle='--', label='Regression')
-  ax.grid()
-  ax.legend()
-  plt.show()
+class Plotter:
+    def __init__(self):
+        pass
+    
+    def linear_regression(self, x: list, y: list, title: str = "Linear Regression", xlabel: str = "x", ylabel: str = "y"):
+        """Linear regression plotter
+
+        Args:
+            x (list): The data for the x-axis.
+            y (list): The data for the y-axis.
+            title (str, optional): The title of the plot. Defaults to "Linear Regression".
+            xlabel (str, optional): The label for the x-axis. Defaults to "x".
+            ylabel (str, optional): The label for the y-axis. Defaults to "y".
+        """
+        df = pd.DataFrame({"x": x, "y": y})
+        x = df[["x"]]
+        y = df[["y"]]
+        
+        clf = LinearRegression()
+        clf.fit(x, y)
+        y_pred = clf.predict(x)
+        m = clf.coef_
+        b = clf.intercept_
+        fig, ax = plt.subplots()
+        ax.scatter(x, y, label="Original", color="red")
+        ax.plot(x, y_pred, label="Regression", color="black")
+        ax.set_title(f"y = {m[0][0]:.3} * x + {b[0]:.3}", loc='right', fontsize=10)
+        fig.suptitle(title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.grid()
+        ax.legend()
+        plt.show()
+
+class Print:
+    def scientific_print(value: float, unit: str = None, name: str = None):
+        """Prints a value in scientific notation
+
+        Args:
+            value (float): The value to print.
+            unit (str): The unit of the value.
+            name (str): The name of the value.
+        """
+        if unit is None and name is not None:
+            print(f"{name} = {value:.3e}")
+            
+        elif name is None and unit is not None:
+            print(f"{value:.3e} {unit}")
+            
+        elif unit is None and name is None:
+            print(f"{value:.3e}")
+            
+        else:
+            print(f"{name} = {value:.3e} {unit}")
+            
+        
+        
